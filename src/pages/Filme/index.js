fa53b470c64620250b0947b9a0c1ca1beb15ecb7
Herisson Silva <herisson.dev@gmail.com>
2022-06-02 18:21:05 -0300

    primeiro commit

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import api from '../../services/api';
+
+function Filme() {
+
+    const { id } = useParams();
+    const [filme, setFilme ] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: '46a466a991f964dc00204c2c9d180d92',
+                    language: 'pt-BR',
+                }
+            }).then((response) => {
+                setFilme(response.data);
+                setLoading(false);
+            }).catch((e) => {
+                console.log(e);
+            })
+        }
+
+        loadFilme();
+
+        return () => {
+            console.log ("componente foi desmontado");
+        }
+    }, [])
+
+    if(loading){
+        return(
+            <div className='loading'>
+                <h2>Carregando detalhes...</h2>
+            </div>
+        )
+    }
+    return (
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src= {`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+            <h3>Sinópse:</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+        </div>
+    )
+}
+
+export default Filme;
